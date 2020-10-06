a0249834d18ba5ed85eaa55213691e0cd8dec491
StarryYJ <yjin23@stevens.edu>
2020-10-06 09:51:18 -0400

    2nd version s Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
 from requests_html import HTMLSession
 import pandas as pd
 
-name, purpose = [], []
+
+def get_info(name, purpose, times):
+    for time in range(times):
+        session = HTMLSession()
+        url = 'http://3.95.249.159:8000/random_company'
+        content = session.get(url)
+        try:
+            i = 1
+            while True:
+                sector = 'body > ol > li:nth-child(' + str(i) + ')'
+                results = content.html.find(sector)
+                text = results[0].text.split()
+                if text[0] == 'Name:':
+                    name.append(' '.join(text[1:]))
+                if text[0] == 'Purpose:':
+                    purpose.append(' '.join(text[1:]))
+                    break
+                i = i + 1
+        except:
+            print("Inappropriate URL at time {0}.".format(time))
+            break
+    print("Successfully collected data.")
 
 
-def get_info():
-    session = HTMLSession()
-    url = 'http://3.95.249.159:8000/random_company'
-    content = session.get(url)
-    i = 1
-    while True:
-        sector = 'body > ol > li:nth-child(' + str(i) + ')'
-        results = content.html.find(sector)
-        text = results[0].text.split()
-        if text[0] == 'Name:':
-            name.append(' '.join(text[1:]))
-        if text[0] == 'Purpose:':
-            purpose.append(' '.join(text[1:]))
-            return 0
-        i = i + 1
+def output_data(name, purpose, filename, existing_file_name: str = None):
+    collect_info = pd.DataFrame({'Name': name, 'Purpose': purpose})
+    try:
+        file = open(filename)
+        file.close()
+        original_info = pd.read_csv(filename)
+        try:
+            pd.concat([original_info, collect_info]).to_csv('./' + filename, index=False)
+            print("Successfully export data to existing file '{0}'.".format(filename))
+        except:
+            print("Inappropriate file to augment.")
+    except:
+        collect_info.to_csv('./' + filename, index=False)
+        print("Successfully export data to a new file named '{0}'.".format(filename))
 
 
-for times in range(50):
-    get_info()
+if __name__ == '__main__':
+    Name, Purpose = [], []
+    get_info(Name, Purpose, 50)
+    # can either augment an existing file or does it always create a new one
+    output_data(Name, Purpose, 'collected_info.csv')
 
-collect_info = pd.DataFrame({'Name': name, 'Purpose': purpose})
-print(collect_info)
+
+
 
 
